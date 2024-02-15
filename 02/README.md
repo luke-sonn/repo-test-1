5caafd66436e7a1e5e27b3863c64f454a3142be5
luke-sonn <112600544+luke-sonn@users.noreply.github.com>
2024-02-15 12:11:50 +0900

    feat: update file by luke

--- 02/README.md
+++ 02/README.md
@@ -1,3 +1,2 @@
 ## 수정된 README.md 2입니다.
-- 또 바꿨어요!!
-- by luke
+- by ilium
--- 02/result.js
+++ 02/result.js
@@ -1,3 +1,3 @@
 console.log('수정된 code.js 2입니다.');
-console.log('또 바꿨어요!!');
-console.log('by luke');
+console.log('또 바꿨어요.');
+console.log('by ilium');
